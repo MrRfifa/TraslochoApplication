1e4fdedd344d4fe110c9b18f095f39496af5b29b
Anouar Rfifa <anouarrafifa99@gmail.com>
2024-03-06 17:45:18 +0100

    Fixed some issues

--- Messaging/Helpers/socket.js
+++ Messaging/Helpers/socket.js
@@ -3,6 +3,8 @@ const { initializeRedis, setWithTTLAsync } = require("../databases/redis");
 const verifyToken = require("../Middleware/authMiddleware");
 const Message = require("../Models/message");
 
+const connectedUsers = new Map(); // Maintaining a list of connected users
+
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
@@ -14,14 +16,18 @@ const initializeSocket = (server) => {
 
   io.on("connection", (socket) => {
     console.log(`User with id ${socket.userId} connected.`);
-    // console.log(socket.userId);
-    setWithTTLAsync(`user_id_${socket.userId}`, socket.userId, 45 * 60);
+    connectedUsers.set(socket.userId, socket.userId); // Add user to connectedUsers map
+    // console.log(connectedUsers);
+    // Emit event to update connected users list
+    io.emit("updateConnectedUsers", Array.from(connectedUsers.keys()));
 
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with id: ${socket.userId} joined room ${data} `);
+    // Joining a room
+    socket.on("join_room", (room) => {
+      socket.join(room);
+      console.log(`User with id: ${socket.userId} joined room ${room} `);
     });
 
+    // Sending a message
     socket.on("send_message", async (data) => {
       const newMessage = new Message({
         contact: data.contact,
@@ -31,12 +37,22 @@ const initializeSocket = (server) => {
       });
       // Save the message to MongoDB
       await newMessage.save();
-      socket.to(data.contact).emit("receive_message", data);
-      // socket.broadcast.emit("receive_message", data);
+
+      // Emit event to notify recipient
+      const recipientSocket = connectedUsers.get(data.contact);
+      if (recipientSocket) {
+        recipientSocket.emit("message_received", newMessage);
+      }
+
+      // Emit event to all sockets in the room
+      io.to(data.contact).emit("receive_message", data);
     });
 
     socket.on("disconnect", async () => {
-      console.log("user diconnected");
+      console.log("User disconnected");
+      connectedUsers.delete(socket.userId); // Remove user from connectedUsers map
+      // Emit event to update connected users list
+      io.emit("updateConnectedUsers", Array.from(connectedUsers.keys()));
     });
   });
 };
