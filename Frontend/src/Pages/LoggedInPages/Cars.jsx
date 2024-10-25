263fa822694aeebdf4e80035df2026f4fee2605c
Anouar Rfifa <anouarrafifa99@gmail.com>
2024-10-25 16:21:59 +0200

    Cars page design done is done, miss functions

--- Frontend/src/Pages/LoggedInPages/Cars.jsx
+++ Frontend/src/Pages/LoggedInPages/Cars.jsx
@@ -1,88 +1,85 @@
-import React from "react";
-
-const Car = () => {
-  const carDetails = {
-    manufacture: "Toyota",
-    model: "Camry",
-    year: 2021,
-    color: "Blue",
-    vehicleType: "Sedan",
-    length: 4.87, // in meters
-    height: 1.45, // in meters
-    isAvailable: true,
-    vehicleImages: [
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg",
-      "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg",
-    ],
-  };
-
+const Cars = () => {
+  //TODO Add models for availability, updating vehicle's data and vehicle images
   return (
-    <div className="max-w-screen-xl mx-auto p-4 ml-0 md:ml-64">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 md:mt-0">
-        {/* Vehicle Characteristics */}
-        <div className="bg-white rounded-lg shadow-lg p-4 md:col-span-1">
-          <h3 className="text-2xl font-bold text-gray-800 mb-2">
-            {carDetails.manufacture} {carDetails.model}
-          </h3>
-          <ul className="space-y-3">
-            <li className="flex justify-between text-gray-700">
-              <span>Year:</span>
-              <span className="font-medium">{carDetails.year}</span>
-            </li>
-            <li className="flex justify-between text-gray-700">
-              <span>Color:</span>
-              <span className="font-medium">{carDetails.color}</span>
-            </li>
-            <li className="flex justify-between text-gray-700">
-              <span>Type:</span>
-              <span className="font-medium">{carDetails.vehicleType}</span>
-            </li>
-            <li className="flex justify-between text-gray-700">
-              <span>Dimensions:</span>
-              <span className="font-medium">
-                {carDetails.length}m (L) x {carDetails.height}m (H)
-              </span>
-            </li>
-            <li className="flex justify-between text-gray-700">
-              <span>Availability:</span>
-              <span
-                className={`font-medium ${
-                  carDetails.isAvailable ? "text-green-500" : "text-red-500"
-                }`}
-              >
-                {carDetails.isAvailable ? "Available" : "Not Available"}
-              </span>
-            </li>
-          </ul>
-        </div>
+    <div className="mx-auto ml-0 md:ml-64 p-6 bg-white rounded-lg grid md:grid-cols-2 grid-rows-1 gap-5">
+      {/* Vehicle Description Section */}
+      <div className="mb-8 text-center mt-10 md:mt-0 p-6 bg-gray-50 shadow-md rounded-lg">
+        <div className="flex flex-col justify-start gap-5 mt-10">
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Year</h2>
+            <p className="text-lg font-medium text-gray-900">2021</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Manufacturer</h2>
+            <p className="text-lg font-medium text-gray-900">Toyota</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Model</h2>
+            <p className="text-lg font-medium text-gray-900">Camry</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Color</h2>
+            <p className="text-lg font-medium text-gray-900">Black</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Vehicle Type</h2>
+            <p className="text-lg font-medium text-gray-900">Van</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Length</h2>
+            <p className="text-lg font-medium text-gray-900">5.5m</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Height</h2>
+            <p className="text-lg font-medium text-gray-900">2.9m</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <h2 className="text-gray-500 text-xl">Availability</h2>
+            <p className="text-lg font-medium text-green-400">Available</p>
+          </div>
+          <div className="flex flex-row text-center justify-between">
+            <button
+              type="button"
+              className="text-white bg-green-600 hover:bg-green-800 hover:scale-105 hover:shadow-xl font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-600 dark:focus:ring-green-600"
+            >
+              Upload images
+            </button>
 
-        {/* Vehicle Image Gallery */}
-        <div className="md:col-span-2 grid grid-cols-2 md:grid-cols-4 gap-4">
-          {carDetails.vehicleImages.map((image, index) => (
-            <div key={index} className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src={image}
-                  alt={`Car Image ${index + 1}`}
-                />
-              </div>
+            <button
+              type="button"
+              className="text-white bg-red-600 hover:bg-red-800 hover:scale-105 hover:shadow-xl font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-600 dark:focus:ring-red-600"
+            >
+              Delete All images
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Gallery Section */}
+      <div className="max-w-6xl mx-auto p-6 bg-gray-50 shadow-md rounded-lg">
+        {/* Gallery Grid */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Repeat for each image */}
+          {[...Array(11)].map((_, index) => (
+            <div key={index}>
+              <img
+                className="h-auto max-w-full rounded-lg hover:scale-105 transition-transform duration-200"
+                src={`https://flowbite.s3.amazonaws.com/docs/gallery/square/image-${
+                  index + 1
+                }.jpg`}
+                alt={`Car image ${index + 1}`}
+              />
             </div>
           ))}
+          <img
+            className="h-auto max-w-full rounded-lg hover:scale-105 transition-transform duration-200"
+            src={`https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg`}
+            alt={`Car image + 1}`}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default Car;
+export default Cars;
