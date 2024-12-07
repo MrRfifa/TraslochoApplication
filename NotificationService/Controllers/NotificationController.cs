d3db7cbcc9e33610f093c61dd40d8fc914af3141
Anouar Rfifa <anouarrafifa99@gmail.com>
2024-12-07 21:13:08 +0100

    removed commented code.

--- NotificationService/Controllers/NotificationController.cs
+++ NotificationService/Controllers/NotificationController.cs
@@ -24,17 +24,6 @@ namespace NotificationService.Controllers
             {
                 return BadRequest(new { success = false, message = "UserId and Content are required." });
             }
-            //TODO See if useless or not: If possible make it simple like if connected send it and then save to the database else save to the db directly without sending
-            // // Retrieve the user's connection ID from Redis
-            // var db = await _redis.GetDatabase();
-            // var connectionId = await db.StringGetAsync($"{request.UserId}-connection");
-
-            // if (!connectionId.HasValue)
-            // {
-            //     // User not connected or no valid connection ID
-            //     return NotFound(new { success = false, message = "User is not currently connected." });
-            // }
-
             // Send notification to a specific user
             await _hubContext.Clients.Client(request.ConnectionId).ReceiveNotification(request);
 
